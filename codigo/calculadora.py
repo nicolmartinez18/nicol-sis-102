8b64dd9044bd65d819fbb863e22e1f75ceceb98c
nicolmartinez18 <nicol.martinez@ucb.edu.bo>
2024-08-14 18:42:38 -0400

    calculadora

---
+++ codigo/calculadora.py
@@ -0,0 +1,12 @@
+#suma
+def suma(x,y):
+    return x + y
+#resta
+def resta(x,y):
+    return x - y
+#multiplicacion
+def multiplicacion (x,y):
+    return x * y
+#division
+def division(x,y):
+    
