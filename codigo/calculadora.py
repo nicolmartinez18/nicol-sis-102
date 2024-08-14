91b4d8bee67f0551e89816addf68d12aae640dcb
nicolmartinez18 <nicol.martinez@ucb.edu.bo>
2024-08-14 19:30:01 -0400

    calculadora

--- codigo/calculadora.py
+++ codigo/calculadora.py
@@ -11,5 +11,15 @@ def multiplicacion (x,y):
 #division
 def division(x,y):
     if y==0:
-        rize 
+        rise ("no se puede dividir por 0")
     return x / y
+print("1.suma")
+print("2.resta")
+print("3.multiplicacion")
+print("4.division")
+opcion= input("ingrese opcion:")
+if opcion=="1":
+    num1=float(input("ingrese el primer numero:"))
+    num2=float(input("ingrese el segundo numero:"))
+    print (f "la suma es:{suma(num1,num2)}")
+elif opcion=="2":
